7c3358f35b91e1f07cac40cece6ead4531f12cc7
DanielCanteroNTNU <97284287+DanielCanteroNTNU@users.noreply.github.com>
2023-01-02 18:15:55 +0100

    Update B66_ContactForce.m

--- B66_ContactForce.m
+++ B66_ContactForce.m
@@ -12,6 +12,12 @@ function [Sol] = B66_ContactForce(Sol,Track,Calc,Train)
 % *** Licensed under the GNU General Public License v3.0                ***
 % *** Author: Daniel Cantero (daniel.cantero@ntnu.no)                   ***
 % *** For help, modifications, and collaboration contact the author.    ***
+% ***                                                                   ***
+% *** If you found this tool useful, please cite:                       ***
+% *** D. Cantero. TTB-2D: Train-Track-Bridge interaction simulation tool***
+% ***   for Matlab, SoftwareX, Volume 20, 2022.                         ***
+% ***   DOI: https://doi.org/10.1016/j.softx.2022.101253                ***
+% ***                                                                   ***
 % *************************************************************************
 
 % -------------------------------------------------------------------------
