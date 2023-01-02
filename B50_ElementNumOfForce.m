24759ba5a2c91234a9dbff450c2d16b9607832c9
DanielCanteroNTNU <97284287+DanielCanteroNTNU@users.noreply.github.com>
2023-01-02 18:14:21 +0100

    Update B50_ElementNumOfForce.m

--- B50_ElementNumOfForce.m
+++ B50_ElementNumOfForce.m
@@ -7,6 +7,12 @@ function [Calc] = B50_ElementNumOfForce(Beam,Calc)
 % *** Licensed under the GNU General Public License v3.0                ***
 % *** Author: Daniel Cantero (daniel.cantero@ntnu.no)                   ***
 % *** For help, modifications, and collaboration contact the author.    ***
+% ***                                                                   ***
+% *** If you found this tool useful, please cite:                       ***
+% *** D. Cantero. TTB-2D: Train-Track-Bridge interaction simulation tool***
+% ***   for Matlab, SoftwareX, Volume 20, 2022.                         ***
+% ***   DOI: https://doi.org/10.1016/j.softx.2022.101253                ***
+% ***                                                                   ***
 % *************************************************************************
 
 % -------------------------------------------------------------------------
