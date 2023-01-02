bf44a029125522377c2694f7d7fcacb077de9be8
DanielCanteroNTNU <97284287+DanielCanteroNTNU@users.noreply.github.com>
2023-01-02 18:16:20 +0100

    Update C01_ModelDeformationPlot.m

--- C01_ModelDeformationPlot.m
+++ C01_ModelDeformationPlot.m
@@ -21,6 +21,12 @@ function [] = C01_ModelDeformationPlot(Calc,Model,Sol,Train)
 % *** Licensed under the GNU General Public License v3.0                ***
 % *** Author: Daniel Cantero (daniel.cantero@ntnu.no)                   ***
 % *** For help, modifications, and collaboration contact the author.    ***
+% ***                                                                   ***
+% *** If you found this tool useful, please cite:                       ***
+% *** D. Cantero. TTB-2D: Train-Track-Bridge interaction simulation tool***
+% ***   for Matlab, SoftwareX, Volume 20, 2022.                         ***
+% ***   DOI: https://doi.org/10.1016/j.softx.2022.101253                ***
+% ***                                                                   ***
 % *************************************************************************
 
 % -------------------------------------------------------------------------
