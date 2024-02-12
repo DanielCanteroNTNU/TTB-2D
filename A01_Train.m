e08607a77adadfbb0cb521a2824b6caefb5d98b7
DanielCanteroNTNU <97284287+DanielCanteroNTNU@users.noreply.github.com>
2024-02-12 09:24:32 +0100

    Update A01_Train.m

--- A01_Train.m
+++ A01_Train.m
@@ -31,48 +31,48 @@ veh_num = 1; run([Train.Load.path,'Manchester_BenchMark']);
 %   each additional vehicle/wagon as follows. Simply define the correct
 %   vehicle number and call the desired vehicle properties
 % Example (uncomment to use)
-% veh_num = 2; run([Train.Load.path,'Manchester_Benchmark']);
-% veh_num = 3; run([Train.Load.path,'Manchester_Benchmark']);
+% veh_num = 2; run([Train.Load.path,'TrainProp_Manchester_Benchmark']);
+% veh_num = 3; run([Train.Load.path,'TrainProp_Manchester_Benchmark']);
 
 % Additional vehicle/wagon properties included in TTB-2D are:
 
 % % AVE S103 ICE3 configuration with 8 coaches
-% veh_num = 1; run([Train.Load.path,'AVE_S103_ICE3_Velaro_E_C1']);
-% veh_num = 2; run([Train.Load.path,'AVE_S103_ICE3_Velaro_E_C2367']);
-% veh_num = 3; run([Train.Load.path,'AVE_S103_ICE3_Velaro_E_C2367']);
-% veh_num = 4; run([Train.Load.path,'AVE_S103_ICE3_Velaro_E_C45']);
-% veh_num = 5; run([Train.Load.path,'AVE_S103_ICE3_Velaro_E_C45']);
-% veh_num = 6; run([Train.Load.path,'AVE_S103_ICE3_Velaro_E_C2367']);
-% veh_num = 7; run([Train.Load.path,'AVE_S103_ICE3_Velaro_E_C2367']);
-% veh_num = 8; run([Train.Load.path,'AVE_S103_ICE3_Velaro_E_C8']);
+% veh_num = 1; run([Train.Load.path,'TrainProp_AVE_S103_ICE3_Velaro_E_C1']);
+% veh_num = 2; run([Train.Load.path,'TrainProp_AVE_S103_ICE3_Velaro_E_C2367']);
+% veh_num = 3; run([Train.Load.path,'TrainProp_AVE_S103_ICE3_Velaro_E_C2367']);
+% veh_num = 4; run([Train.Load.path,'TrainProp_AVE_S103_ICE3_Velaro_E_C45']);
+% veh_num = 5; run([Train.Load.path,'TrainProp_AVE_S103_ICE3_Velaro_E_C45']);
+% veh_num = 6; run([Train.Load.path,'TrainProp_AVE_S103_ICE3_Velaro_E_C2367']);
+% veh_num = 7; run([Train.Load.path,'TrainProp_AVE_S103_ICE3_Velaro_E_C2367']);
+% veh_num = 8; run([Train.Load.path,'TrainProp_AVE_S103_ICE3_Velaro_E_C8']);
 
 % % Eurostar configuration
-% veh_num = 1; run([Train.Load.path,'Eurostar_Locomotive']);
-% veh_num = 2; run([Train.Load.path,'Eurostar_EC1']);
-% veh_num = 3; run([Train.Load.path,'Eurostar_EC2_EC8']);
-% veh_num = 4; run([Train.Load.path,'Eurostar_EC2_EC8']);
-% veh_num = 5; run([Train.Load.path,'Eurostar_EC2_EC8']);
-% veh_num = 6; run([Train.Load.path,'Eurostar_EC2_EC8']);
-% veh_num = 7; run([Train.Load.path,'Eurostar_EC2_EC8']);
-% veh_num = 8; run([Train.Load.path,'Eurostar_EC2_EC8']);
-% veh_num = 9; run([Train.Load.path,'Eurostar_EC2_EC8']);
-% veh_num = 10; run([Train.Load.path,'Eurostar_EC9']);
-% veh_num = 11; run([Train.Load.path,'Eurostar_Locomotive']);
+% veh_num = 1; run([Train.Load.path,'TrainProp_Eurostar_Locomotive']);
+% veh_num = 2; run([Train.Load.path,'TrainProp_Eurostar_EC1']);
+% veh_num = 3; run([Train.Load.path,'TrainProp_Eurostar_EC2_EC8']);
+% veh_num = 4; run([Train.Load.path,'TrainProp_Eurostar_EC2_EC8']);
+% veh_num = 5; run([Train.Load.path,'TrainProp_Eurostar_EC2_EC8']);
+% veh_num = 6; run([Train.Load.path,'TrainProp_Eurostar_EC2_EC8']);
+% veh_num = 7; run([Train.Load.path,'TrainProp_Eurostar_EC2_EC8']);
+% veh_num = 8; run([Train.Load.path,'TrainProp_Eurostar_EC2_EC8']);
+% veh_num = 9; run([Train.Load.path,'TrainProp_Eurostar_EC2_EC8']);
+% veh_num = 10; run([Train.Load.path,'TrainProp_Eurostar_EC9']);
+% veh_num = 11; run([Train.Load.path,'TrainProp_Eurostar_Locomotive']);
 
 % % Chinese Star (Exmaple: Locomotive + 3 wagons)
-% veh_num = 1; run([Train.Load.path,'ChineseStarPowerCar']);
-% veh_num = 2; run([Train.Load.path,'DoubleDeckPassengerCoach']);
-% veh_num = 3; run([Train.Load.path,'DoubleDeckPassengerCoach']);
-% veh_num = 4; run([Train.Load.path,'DoubleDeckPassengerCoach']);
+% veh_num = 1; run([Train.Load.path,'TrainProp_ChineseStarPowerCar']);
+% veh_num = 2; run([Train.Load.path,'TrainProp_DoubleDeckPassengerCoach']);
+% veh_num = 3; run([Train.Load.path,'TrainProp_DoubleDeckPassengerCoach']);
+% veh_num = 4; run([Train.Load.path,'TrainProp_DoubleDeckPassengerCoach']);
 
 % % Pioneer M vehicle
-% veh_num = 1; run([Train.Load.path,'Pioneer_M_vehicle']);
+% veh_num = 1; run([Train.Load.path,'TrainProp_Pioneer_M_vehicle']);
 
 % % Pioneer R vehicle
-% veh_num = 1; run([Train.Load.path,'Pioneer_R_vehicle']);
+% veh_num = 1; run([Train.Load.path,'TrainProp_Pioneer_R_vehicle']);
 
 % % Shinkansen S300
-% veh_num = 1; run([Train.Load.path,'ShinkansenS300']);
+% veh_num = 1; run([Train.Load.path,'TrainProp_ShinkansenS300']);
 
 % Copy of Train.vel
 Train.Veh(1).vel = Train.vel;
